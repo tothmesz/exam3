8224313e6fea2e4b319d0827e5ec39b2c7f45b69
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-04-29 12:52:40 +0200

    done

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,34 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        try {
+            File myObj = new File(output);
+            FileWriter myWriter = new FileWriter(output);
+            Iterator itr = contacts.entrySet().iterator();
+            String data = new String("");
+            while (itr.hasNext()){
+                Map.Entry entry = (Map.Entry) itr.next();
+                String name = (String) entry.getKey();
+                String number = (String) entry.getValue();
+                if (!data.equals("")) {
+                    data += "\n";
+                }
+                data += name + ": " + number;
+            }
+            myWriter.write(data);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred ");
+        }
+    }
+
+}
